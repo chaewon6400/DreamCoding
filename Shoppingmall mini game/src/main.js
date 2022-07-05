0fc5d514d5fc315e05203cb7e2c09f48232cd2f5
전채원 <chaewon6400@gmail.com>
2022-07-06 00:57:29 +0900

    setEventListeners end

--- Shoppingmall mini game/src/main.js
+++ Shoppingmall mini game/src/main.js
@@ -1,36 +1,35 @@
-// JSON file로부터 items를 동적으로 받아오는 메서드
+// Fetch the items from the JSON file
 function loadItems() {
   return fetch("data/data.json")
     .then((response) => response.json())
     .then((json) => json.items);
 }
+
 // Update the list with the given items
 function displayItems(items) {
   const container = document.querySelector(".items");
-  // 콘솔에 한 번 찍어보기.
-  // const html = items.map((item) => createHTMLString(item)).join("");
-  // console.log(html);
   container.innerHTML = items.map((item) => createHTMLString(item)).join("");
 }
 
 // Create HTML list item from the given data item
 function createHTMLString(item) {
   return `
-<li class="item">
-<img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
-<span class="item__description">${item.gender}, ${item.size}</span>
-</li>
-`;
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
+        <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
 }
 
 function onButtonClick(event, items) {
   const dataset = event.target.dataset;
-  const key = event.target.dataset.key;
-  const value = event.target.dataset.value;
+  const key = dataset.key;
+  const value = dataset.value;
 
   if (key == null || value == null) {
     return;
   }
+
   displayItems(items.filter((item) => item[key] === value));
 }
 
