b49485d92af33c6a5afa50515cb631ad904fe291
전채원 <chaewon6400@gmail.com>
2022-07-14 01:54:24 +0900

    handleIncrement, handleDecrement

--- React/react-basic/habit-tracker/src/components/habits.jsx
+++ React/react-basic/habit-tracker/src/components/habits.jsx
@@ -9,16 +9,33 @@ class Habits extends Component {
       { id: 3, name: "Coding", count: 0 },
     ],
   };
+
   handleIncrement = (habit) => {
     console.log(`handleIncrement ${habit}`);
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+
+    this.setState({ habits: habits });
+    // 위에 같이 키와 밸류가 동일한 이름을 갖고 있는 경우에는 하나로 생략 가능
+    // -> this.setState({ habits })
   };
 
   handleDecrement = (habit) => {
-    console.log(`handleDecrement ${habit}`);
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    habits[index].count = count < 0 ? 0 : count;
+
+    this.setState({ habits: habits });
   };
 
   handleDelete = (habit) => {
-    console.log(`handleDelete ${habit}`);
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+
+    this.setState({ habits: habits });
   };
 
   render() {
