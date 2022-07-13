b47b2a2362a70d09098463aa39c82a937bd3d771
전채원 <chaewon6400@gmail.com>
2022-07-13 18:15:56 +0900

    Update app.jsx

--- React/react-basic/habit-tracker/src/app.jsx
+++ React/react-basic/habit-tracker/src/app.jsx
@@ -2,7 +2,18 @@ import React from "react";
 import "./app.css";
 
 function App() {
-  return <h1>Hello :)</h1>;
+  const name = undefined;
+
+  return (
+    // jsx에서 형제노드를 함께 쓰려면 묶어줘야함
+    <>
+      <h1>Hi~</h1>
+      {name && <h1>Hello {name} :)</h1>}
+      {["🌱", "🌙"].map((item) => (
+        <h1>{item}</h1>
+      ))}
+    </>
+  );
 }
 
 export default App;
