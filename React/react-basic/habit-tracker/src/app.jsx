52512c681512a53dd7b5599f796cb6c21bbc7e86
전채원 <chaewon6400@gmail.com>
2022-07-15 00:10:24 +0900

    Update app.jsx

--- React/react-basic/habit-tracker/src/app.jsx
+++ React/react-basic/habit-tracker/src/app.jsx
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 import "./app.css";
 import Habits from "./components/habits";
 
-function App() {
-  return <Habits />;
+class App extends Component {
+  state = {
+    habits: [
+      { id: 1, name: "Reading", count: 0 },
+      { id: 2, name: "Running", count: 0 },
+      { id: 3, name: "Coding", count: 0 },
+    ],
+  };
+  handleIncrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+
+    this.setState({ habits: habits });
+    // 위에 같이 키와 밸류가 동일한 이름을 갖고 있는 경우에는 하나로 생략 가능
+    // -> this.setState({ habits })
+  };
+
+  handleDecrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    habits[index].count = count < 0 ? 0 : count;
+    this.setState({ habits });
+  };
+
+  handleDelete = (habit) => {
+    const habits = this.state.habits.filter((item) => item.id !== habit.id);
+    this.setState({ habits });
+  };
+
+  render() {
+    return (
+      <Habits
+        habits={this.state.habits}
+        onIncrement={this.handleIncrement}
+        onDecrement={this.handleDecrement}
+        onDelete={this.handleDelete}
+      />
+    );
+  }
 }
 
 export default App;
--- React/react-basic/habit-tracker/src/components/habits.jsx
+++ React/react-basic/habit-tracker/src/components/habits.jsx
@@ -2,41 +2,22 @@ import React, { Component } from "react";
 import Habit from "./habit";
 
 class Habits extends Component {
-  state = {
-    habits: [
-      { id: 1, name: "Reading", count: 0 },
-      { id: 2, name: "Running", count: 0 },
-      { id: 3, name: "Coding", count: 0 },
-    ],
-  };
-
   handleIncrement = (habit) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    habits[index].count++;
-
-    this.setState({ habits: habits });
-    // 위에 같이 키와 밸류가 동일한 이름을 갖고 있는 경우에는 하나로 생략 가능
-    // -> this.setState({ habits })
+    this.props.onIncrement(habit);
   };
 
   handleDecrement = (habit) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    const count = habits[index].count - 1;
-    habits[index].count = count < 0 ? 0 : count;
-    this.setState({ habits: habits });
+    this.props.onDecrement(habit);
   };
 
   handleDelete = (habit) => {
-    const habits = this.state.habits.filter((item) => item.id !== habit.id);
-    this.setState({ habits: habits });
+    this.props.onDelete(habit);
   };
 
   render() {
     return (
       <ul>
-        {this.state.habits.map((habit) => (
+        {this.props.habits.map((habit) => (
           <Habit
             key={habit.id}
             habit={habit}
